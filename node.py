13abb5c4cf3deaf981915cfecf93ea555d9bcff9
Pedro Rodrigues <prodrigues1990@gmail.com>
2018-08-12 06:24:45 +0100

    new node class, loading waypoints file

---
+++ node.py
@@ -0,0 +1,80 @@
+#!/usr/bin/bash/python3
+
+from collections import defaultdict
+from geopy import distance
+from csv import reader as csvreader
+
+NODES = defaultdict(list)
+
+class Node(object):
+    def __init__(self, name, lat, lon, region):
+        self.name = name
+        self.location = (float(lat), float(lon))
+        self.region = region
+
+        self.g_cost = 0.0
+        self.h_cost = 0.0
+        self.via = 'DCT'
+
+        NODES[name].append(self)
+
+    def load(name, *args, **kwargs):
+        if name not in NODES:
+            raise LookupError('%s did not match any node' % name)
+        
+        candidates = NODES[name]
+
+        if len(candidates) == 1:
+            return candidates[0]
+
+        location = None
+        if 'lat' in kwargs and 'lon' in kwargs:
+            location = (float(kwargs['lat']), float(kwargs['lon']))
+        if 'location' in kwargs:
+            location = kwargs['location']
+
+        if not location:
+            raise LookupError('Multiple matches for %s' % name)
+
+        candidates_by_distance = [
+                (node, distance.vincenty(node.location, location))
+                for node in candidates]
+
+        return min(candidates_by_distance, key=lambda x: x[1])[0]
+
+    @property
+    def f_cost(self):
+        return self.g_cost + self.h_cost
+
+    @property
+    def key(self):
+        return ','.join([
+                'S',
+                self.name,
+                '{:.5f}'.format(self.lat),
+                '{:.5f}'.format(self.lon)])
+
+    def long_route(self):
+        if not self.parent:
+            return self.name
+        return ' '.join([self.parent.long_route(), self.via, self.name])
+
+    def short_route(self, via=None):
+        if not self.parent:
+            return self.name
+
+        if not via or via != self.via:
+            return ' '.join([self.parent.short_route(via=self.via),
+                                                     self.via,
+                                                     self.name])
+        return self.parent.short_route(via=self.via)
+
+    def distance_to(self, node):
+        return distance.vincenty(self.location, node.location).nm
+
+with open('navdata/Waypoints.txt') as f:
+    reader = csvreader(f, delimiter=',')
+    for line in reader:
+        if len(line) == 0:
+            continue
+        Node(*line)
